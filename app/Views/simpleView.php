a1f3623fc4b644c60576bc37b8ffa3c0e5b0d971
Debashish Raut <debashish.adcs@gmail.com>
2025-12-13 20:19:06 +0530

    Some fixation.

---
+++ app/Views/simpleView.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First CI4 Page</title>
+    <style>
+        body { font-family: sans-serif; text-align: center; padding: 50px; }
+        h1 { color: #dd4814; }
+    </style>
+</head>
+<body>
+
+    <h1>Hello, Developer!</h1>
+    
+    <p>This is my first custom view in CodeIgniter 4.</p>
+    
+    <p>Current Time: <?php echo date('H:i:s'); ?></p>
+
+</body>
+</html>
